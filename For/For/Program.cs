12fb8073a4bee40cfb14f080522f61467993fbbf
juperameza <juperameza@hotmail.com>
2019-08-22 14:05:45 -0500

    Confirmo

---
+++ For/For/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.Write("Dame el inicio del ciclo ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Dame el final del ciclo ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("De cuanto en cuanto quieres que avance el ciclo? ");
+            z = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i=x;i>=y;i-=z)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                for (int i = x; i <= y; i+=z)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
